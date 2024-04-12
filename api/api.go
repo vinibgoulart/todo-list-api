a27b8aa77e241e501ee8a0fe97233b0cfa2a0efd
Vinicius <viblaziusgoulart@gmail.com>
2024-04-11 22:03:31 -0300

    fix(api): route path

--- api/api.go
+++ api/api.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/vinibgoulart/todo-list/user"
+	"github.com/vinibgoulart/todo-list/modules/user"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
