ee3bdb6333c75ccd316ca88a65c895f1fa50ba90
Vinicius <viblaziusgoulart@gmail.com>
2024-04-18 20:14:38 -0300

    fix(server): allow patch in cors

--- http/server.go
+++ http/server.go
@@ -26,7 +26,7 @@ func SetupHttpServer(db *sql.DB) chi.Router {
 
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
